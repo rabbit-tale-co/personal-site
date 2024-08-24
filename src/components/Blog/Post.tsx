2556ab8d8209cb58d3d704d90f7ecfe1457237b1
poker-cats-co <hasiradoo@starowl.dev>
2024-08-24 13:50:53 +0200

    updated older posts

--- src/components/Blog/Post.tsx
+++ src/components/Blog/Post.tsx
@@ -1,19 +1,19 @@
 import Link from 'next/link'
 
 type PostProps = {
-  date: string
-  href: string
-  title: string
+	date: string
+	href: string
+	title: string
 }
 
 const Post = ({ date, href, title }: PostProps) => (
-  <Link
-    href={href}
-    className="mb-6 flex w-full items-baseline justify-between border-b border-zinc-200 py-2 transition-colors duration-200 ease-out hover:border-zinc-400"
-  >
-    <p className="text-balance font-medium">{title}</p>
-    <p className="shrink-0 text-sm text-zinc-600">{date}</p>
-  </Link>
+	<Link
+		href={href}
+		className='mb-6 p-6 flex w-full items-baseline justify-between transition-colors rounded-2xl duration-200 ease-out bg-zinc-800 hover:bg-zinc-900'
+	>
+		<p className='text-balance font-medium text-zinc-50'>{title}</p>
+		<p className='shrink-0 text-sm text-zinc-400'>{date}</p>
+	</Link>
 )
 
 export default Post
